990270d1772f9e472d2e538268921b8f4acbe6bf
manel turki <menelturki@gmail.com>
2020-01-22 18:29:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 // Write your code here!
-let element = document.createElement('div')
-document.body.appendChild(element)
-ul.removeChild(ul.querySelector('li:nth-child(2)'))
-document.querySelector('main#main').remove();
+document.querySelector("main#main").remove();
